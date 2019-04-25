175e5ee16af1c268594f3f36b50fc1921f8a7dc1
gazblair <49972708+gazblair@users.noreply.github.com>
2019-04-25 07:54:04 +0100

    Refactor Account from anemic to rich domain model

--- src/MoneyBox.App.UnitTest/AccountBuilder.cs
+++ src/MoneyBox.App.UnitTest/AccountBuilder.cs
@@ -1,50 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Moneybox.App;
-using Moneybox.App.DataAccess;
-using Moq;
+﻿using Moneybox.App;
+using System;
 
 namespace moneybox_unit_tests
 {
     public class AccountBuilder
     {
-        private Account account = new Account();
+        private Guid id;
+        private User user = new User();
+        private decimal balance;
+        private decimal paidIn;
 
         public AccountBuilder(Guid id)
         {
-            account.Id = id;
-            account.User = new User();
-        }
-
-        public AccountBuilder WithWithdrawn(decimal withdrawn)
-        {
-            account.Withdrawn = withdrawn;
-            return this;
-        }
-
-
-        public AccountBuilder WithBalance(decimal balance)
-        {
-            account.Balance = balance;
-            return this;
+            this.id = id;
         }
 
         public AccountBuilder WithPaidInAmount(decimal paidIn)
         {
-            account.PaidIn = paidIn;
+            this.paidIn = paidIn;
+            return this;
+        }
+
+        public AccountBuilder WithBalance(decimal paidIn)
+        {
+            this.balance = paidIn;
             return this;
         }
 
         public AccountBuilder WithUserEmail(string email)
         {
-            account.User.Email = email;
+            user.Email = email;
             return this;
         }
 
         public Account Build()
         {
-            return account;
+            return new Account(id, user, balance, 0, paidIn);
         }
     }
 }
--- src/Moneybox.App/Domain/Account.cs
+++ src/Moneybox.App/Domain/Account.cs
@@ -1,19 +1,67 @@
 ﻿using System;
+using Moneybox.App.Domain.Services;
 
 namespace Moneybox.App
 {
     public class Account
     {
         public const decimal PayInLimit = 4000m;
+        public const decimal FivePound = 500m;
+        public Account(Guid id,
+            User user,
+            decimal balance,
+            decimal withdrawn,
+            decimal paidIn)
+        {
+            Id = id;
+            User = user;
+            Balance = balance;
+            Withdrawn = withdrawn;
+            PaidIn = paidIn;
+        }
 
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
-        public User User { get; set; }
+        public User User { get; private set; }
 
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
 
-        public decimal Withdrawn { get; set; }
+        public decimal Withdrawn { get; private set; }
 
-        public decimal PaidIn { get; set; }
+        public decimal PaidIn { get; private set; }
+
+        public void PayIn(decimal amountInPence, INotificationService notificationService)
+        {
+            var newPaidIn = PaidIn + amountInPence;
+            if (newPaidIn > Account.PayInLimit)
+            {
+                throw new InvalidOperationException("Account pay in limit reached");
+            }
+
+            if (Account.PayInLimit - newPaidIn < FivePound)
+            {
+                notificationService.NotifyApproachingPayInLimit(User.Email);
+            }
+
+            Balance = Balance + amountInPence;
+            PaidIn = newPaidIn;
+        }
+
+        public void Withdraw(decimal amountInPence, INotificationService notificationService)
+        {
+            var newBalance = Balance - amountInPence;
+            if (newBalance < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+            }
+
+            if (newBalance < FivePound)
+            {
+                notificationService.NotifyFundsLow(User.Email);
+            }
+
+            Balance = newBalance;
+            Withdrawn = Withdrawn - amountInPence;
+        }
     }
 }
